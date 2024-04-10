f3520c7ba032e13ebe4fc31fdaf4165d8cad36c6
엄혜영 <112528738+EomHyeYeong@users.noreply.github.com>
2024-04-10 22:59:40 +0900

    [Bronze III] Title: 369, Time: 784 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -4,10 +4,9 @@ clabable = ['3', '6', '9']
 
 map(lambda x: x==j, clabable)
 for i in range(1, n+1):
-    nsplit = list(str(i))
-    if map(lambda x: x in nsplit, clabable):
-        for j in nsplit:
-            if j=='3' or j=='6' or j=='9':
-                clapCnt += 1
+    number = str(i)
+    clapCnt += number.count('3')
+    clapCnt += number.count('6')
+    clapCnt += number.count('9')
 
 print(clapCnt)
