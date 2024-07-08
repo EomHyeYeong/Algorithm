5486ce9c3f35754f23f3ef237d620b19ede8d6c1
엄혜영 <112528738+EomHyeYeong@users.noreply.github.com>
2024-07-08 23:00:03 +0900

    [Bronze V] Title: 이상한 기호, Time: 208 ms, Memory: 17688 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -4,11 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		long a = sc.nextInt();
+		long b = sc.nextInt();
 		
 		long result = (a+b) * (a-b);
 		System.out.println(result);
 	}
-
 }
