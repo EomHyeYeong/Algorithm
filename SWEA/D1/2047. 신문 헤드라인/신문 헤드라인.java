2d3ec43fd1e9c1d79f0a9df5b151a55c98a44bff
엄혜영 <112528738+EomHyeYeong@users.noreply.github.com>
2024-10-18 17:55:20 +0900

    [D1] Title: 신문 헤드라인, Time: 106 ms, Memory: 19,272 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String newspaper = br.readLine().toUpperCase();
+        System.out.println(newspaper);
+	}
+}
