d7292e14b8bc61ddbe19a291e63068841503987a
엄혜영 <112528738+EomHyeYeong@users.noreply.github.com>
2025-09-17 16:21:57 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.53 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,37 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        HashMap<String, Integer> wordMap = setWordMap();
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < s.length(); i++) {
-            if(Character.isDigit(s.charAt(i))) {
-                sb.append(s.charAt(i));
-                continue;
-            }
-
-            for(String number : wordMap.keySet()) {
-                if(s.substring(i).startsWith(number)) {
-                    sb.append(wordMap.get(number));
-                    break;
-                }
-            }
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-        return Integer.parseInt(sb.toString());
-    }
-    
-    HashMap<String, Integer> setWordMap() {
-        HashMap<String, Integer> wordMap = new HashMap<>();
-        wordMap.put("zero", 0);
-        wordMap.put("one", 1);
-        wordMap.put("two", 2);
-        wordMap.put("three", 3);
-        wordMap.put("four", 4);
-        wordMap.put("five", 5);
-        wordMap.put("six", 6);
-        wordMap.put("seven", 7);
-        wordMap.put("eight", 8);
-        wordMap.put("nine", 9);
-        return wordMap;
+        return Integer.parseInt(s);
     }
 }
